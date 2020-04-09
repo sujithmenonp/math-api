f65a314816c0584cae1d99f8e26ad038a02f15b9
Sujith Menon <sujithmenonp@gmail.com>
2020-04-09 16:24:43 +0530

    Fixed bug with basic authentication

--- src/main/java/math/api/config/WebSecurityConfig.java
+++ src/main/java/math/api/config/WebSecurityConfig.java
@@ -2,12 +2,13 @@ package math.api.config;
 
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -15,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
        http.csrf().disable()
                .authorizeRequests()
                .antMatchers("/api").authenticated()
-               .anyRequest().permitAll()
+               .anyRequest().authenticated()
                .and().httpBasic();
     }
 
--- src/test/java/math/api/IntegrationTest.java
+++ src/test/java/math/api/IntegrationTest.java
@@ -5,13 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import math.api.model.request.AddRequest;
 import math.api.model.request.DiffRequest;
 import math.api.model.response.AddResponse;
 import math.api.model.response.DiffResponse;
+
+import javax.xml.bind.DatatypeConverter;
+import java.util.Arrays;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
@@ -30,7 +31,8 @@ public class IntegrationTest {
         HttpEntity<AddRequest> entity = new HttpEntity<>(request);
         String uri="http://localhost:"+port+"/api/add";
 
-        ResponseEntity<AddResponse> responseEntity = testRestTemplate.postForEntity(uri ,
+        ResponseEntity<AddResponse> responseEntity = testRestTemplate.withBasicAuth
+                ("admin","password").postForEntity(uri ,
                 entity, AddResponse.class);
 
         assert responseEntity.getBody().getResult().equals("30");
@@ -39,13 +41,16 @@ public class IntegrationTest {
 
     @Test
     void testHttpPostDiffCall(){
+        String authorizationHeader = "Basic " + DatatypeConverter.printBase64Binary(("admin" + ":" + "password").getBytes());
+
         DiffRequest request = new DiffRequest();
         request.setX("10");
         request.setY("20");
         HttpEntity<DiffRequest> entity = new HttpEntity<>(request);
         String uri="http://localhost:"+port+"/api/diff";
 
-        ResponseEntity<DiffResponse> responseEntity = testRestTemplate.postForEntity(uri ,
+
+        ResponseEntity<DiffResponse> responseEntity = testRestTemplate.withBasicAuth("admin","password").postForEntity(uri ,
                 entity, DiffResponse.class);
 
         assert responseEntity.getBody().getResult().equals("-10");
@@ -60,7 +65,8 @@ public class IntegrationTest {
         HttpEntity<DiffRequest> entity = new HttpEntity<>(request);
         String uri="http://localhost:"+port+"/api/add";
 
-        ResponseEntity<AddResponse> responseEntity = testRestTemplate.postForEntity(uri ,
+        ResponseEntity<AddResponse> responseEntity = testRestTemplate.withBasicAuth(
+                "admin","password").postForEntity(uri ,
                 entity, AddResponse.class);
 
         assert responseEntity.getStatusCode()== HttpStatus.BAD_REQUEST;
